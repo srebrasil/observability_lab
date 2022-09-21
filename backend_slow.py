047474add5b5aac3601e331eb6cab4d56ed2fb07
rcsousa <ricardo.c.sousa@gmail.com>
2022-09-21 18:25:17 -0300

    major update

--- backend_slow.py
+++ backend_slow.py
@@ -3,28 +3,34 @@
 from flask import Flask, jsonify
 #expose endpoint to return the operation of class Calc
 from classes.Calc import Calc
+from random import randint
+from time import sleep
 
 app = Flask(__name__) # create the Flask app
 
 @app.route('/api/v1/calc/sum/<int:num1>/<int:num2>', methods=['GET']) # route to expose the api
 def get_sum(num1, num2):
+    sleep(randint(6, 10))
     calc = Calc(num1, num2)
     return jsonify({'result': calc.sum()})
 
 @app.route('/api/v1/calc/sub/<int:num1>/<int:num2>', methods=['GET']) # route to expose the api
 def get_sub(num1, num2):
+    sleep(randint(6, 10))
     calc = Calc(num1, num2)
     return jsonify({'result': calc.sub()})
 
 @app.route('/api/v1/calc/mult/<int:num1>/<int:num2>', methods=['GET']) # route to expose the api
 def get_mult(num1, num2):
+    sleep(randint(6, 10))
     calc = Calc(num1, num2)
     return jsonify({'result': calc.mult()})
 
 @app.route('/api/v1/calc/div/<int:num1>/<int:num2>', methods=['GET']) # route to expose the api
 def get_div(num1, num2):
+    sleep(randint(6, 10))
     calc = Calc(num1, num2)
     return jsonify({'result': calc.div()})
 
 if __name__ == '__main__': # run the app
-    app.run(debug=True, host='0.0.0.0', port='4000') # run the app
+    app.run(debug=True, host='0.0.0.0', port='4003') # run the app
---
+++ frontend.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import requests
+import random
+
+app = Flask(__name__)
+
+operations = ['sum', 'sub', 'mult', 'div']
+ports = {'v1': 4001, 'v2': 4002, 'v3': 4003}
+ 
+
+@app.route('/api/v1/frontend', methods=['GET'])
+def get_test():
+    response = requests.get('http://localhost:'+str(random.choice(list(ports.values())))+'/api/v1/calc/'+ random.choice(operations) +'/' + str(random.randint(1, 1000)) + '/' + str(random.randint(1, 1000)))
+    if response.status_code == 200:
+        combined_response = requests.get('http://localhost:'+str(random.choice(list(ports.values())))+'/api/v1/calc/'+ random.choice(operations) +'/' + str(random.randint(1, 1000)) + '/' + str(random.randint(1, 1000)))
+    return jsonify({'Result1' : response.json()['result'], 'Result2' : combined_response.json()['result']})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=4000)
